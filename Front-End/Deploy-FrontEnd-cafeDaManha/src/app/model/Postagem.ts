3547c0254654abaf91249c2c92764daab2e4db4b
GustavoHLC <gutohenri_car@hotmail.com>
2022-01-08 14:46:28 -0300

    Desafio Unidac

--- Front-End/Deploy-FrontEnd-cafeDaManha/src/app/model/Postagem.ts
+++
@@ -1,8 +0,0 @@
-import { Usuario } from "./Usuario"
-
-export class Postagem{
-  public id: number
-  public item: string
-  public data: Date
-  public usuario: Usuario
-}
--- Front-End/Deploy-FrontEnd-cafeDaManha/src/app/model/UsuarioLogin.ts
+++
@@ -1,8 +0,0 @@
-export class UsuarioLogin{
-  public id: number
-  public nome: string
-  public cpf: string
-  public senha: string
-  public foto: string
-  public token: string
-}
